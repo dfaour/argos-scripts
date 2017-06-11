8f158c444ae0c7e8571752b75e4492f3304206b5
David Faour <david@faour.ca>
2017-06-10 21:13:53 -0300

    Added error handling; other small improvements.

--- getnews.py
+++ getnews.py
@@ -5,15 +5,23 @@ import feedparser
 #Change to your favourite news RSS
 feed_url = "https://news.google.ca/news?cf=all&hl=en&pz=1&ned=ca&output=rss"
 
-feed = feedparser.parse(feed_url)
+#Number of stories to get
+stories = 10
 
 print("News")
 print("---")
 
+feed = feedparser.parse(feed_url)
 
-for i in range(10):
-    print(feed['entries'][i]['title'] + " | href='" + feed['entries'][i]['link'] + "'")
+try:
+    for i in range(stories):
+        print(feed['entries'][i]['title'] + " | href='" + feed['entries'][i]['link'] + "'")
 
+except:
+    print("[Connection error...]")
+    print("---")
+    print("Go to Google News... | href='https://news.google.ca'")
+    exit()
 print("---")
 
 #Change to your favourite news site
--- getquotes.py
+++ getquotes.py
@@ -5,23 +5,27 @@ import sys
 
 stocks = ['GOOG', 'RHT']
 
-for i in stocks:
-    price = Share(i)
-    name = i
-    if len(i) > 4:
-        name = i[:4]
-    price2 = price.get_price()
-    if len(price2) > 6:
-        price2 = price2[:6]
-    change = price.get_change()
-    if len(change) > 5:
-        change = change[:5]
-    print (name + ": " + price2 + " (", end="")
-    if price.get_change()[:1] == "+":
-        print("\033[1;32m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
-    elif price.get_change()[:1] == "-":
-        print("\033[1;31m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
+try:
+    for i in stocks:
+        price = Share(i)
+        name = i
+        if len(i) > 4:
+            name = i[:4]
+        price2 = price.get_price()
+        if len(price2) > 6:
+            price2 = price2[:6]
+        change = price.get_change()
+        if len(change) > 5:
+            change = change[:5]
+        print (name + ": " + price2 + " (", end="")
+        if price.get_change()[:1] == "+":
+            print("\033[1;32m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
+        elif price.get_change()[:1] == "-":
+            print("\033[1;31m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
 
-#If you use yahoo finance watchlist...
-#print("---")
-#print("Portfolio... | href='https://ca.finance.yahoo.com/portfolio/p_0/view/v1'")
+    #If you use yahoo finance watchlist...
+    #print("---")
+    #print("Portfolio... | href='https://ca.finance.yahoo.com/portfolio/p_0/view/v1'")
+
+except:
+    print("[Connection error...]")
