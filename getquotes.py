0badc3390f05d409a78cc5ed44f600c6d0636466
David Faour <david@faour.ca>
2017-06-13 12:51:20 -0300

    Bugfixes; added option to show trade times

--- getquotes.py
+++ getquotes.py
@@ -5,6 +5,12 @@ import sys
 
 stocks = ['GOOG', 'RHT']
 
+#set to 0 if you don't want to see trade time in the drop-down menu
+times = 1
+
+#set to 0 if you don't want to see a link to your Yahoo Finance portfolio
+portfolio = 1
+
 try:
     for i in stocks:
         price = Share(i)
@@ -19,13 +25,23 @@ try:
             change = change[:5]
         print (name + ": " + price2 + " (", end="")
         if price.get_change()[:1] == "+":
-            print("\033[1;32m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
+            print("\033[1;32m" + change + "\033[0m) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
         elif price.get_change()[:1] == "-":
-            print("\033[1;31m" + change + "\033[0;37m ) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
+            print("\033[1;31m" + change + "\033[0m) | href='https://ca.finance.yahoo.com/quote/" + i + "' refresh=true")
 
-    #If you use yahoo finance watchlist...
-    #print("---")
-    #print("Portfolio... | href='https://ca.finance.yahoo.com/portfolio/p_0/view/v1'")
+    if times == 1:
+        print("---")
+        print("Trade Times")
+        for i in stocks:
+            price = Share(i)
+            name = i
+            if len(i) > 4:
+                name = i[:4]
+            print(name + ": " + price.get_trade_datetime())
+
+    if portfolio == 1:
+        print("---")
+        print("Portfolio... | href='https://ca.finance.yahoo.com/portfolio/p_0/view/v1'")
 
 except:
-    print("[Connection error] | refresh=true")
+    print("[Error] | refresh=true")
